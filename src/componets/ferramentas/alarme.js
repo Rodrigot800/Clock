a3f47d2e6eb270d6ddaff50e7eacc3780864ea10
Rodrigo <rodrigopeixotot800@gmail.com>
2024-10-16 22:42:02 -0300

    testando circulo de progresso

--- src/componets/ferramentas/alarme.js
+++ src/componets/ferramentas/alarme.js
@@ -2,14 +2,28 @@ import React, { useState, useEffect } from "react";
 
 // Componente Cronômetro
 function Alarme() {
+  function go() {
+    var circle = document.querySelector("#proc");
+    var n = Number(document.querySelector("#pro").value) ;
+    document.querySelector(".number").innerHTML = n + '%'
+    circle.style.strokeDashoffset = (440 - (440 * n) / 100)
+  }
   return (
-    <div className=" box teste">
-      <div className="box-circle">
-        <svg>
-          <circle cx={70} cy={70} r={70}></circle>
-          <circle cx={70} cy={70} r={70}></circle>
-        </svg>
-        <h2>70%</h2>
+    <div>
+      <div className="btnen">
+        <input id="pro" type="number" />
+        <button onClick={go}>enviar</button>
+      </div>
+      <div className="box teste">
+        <div className="box-circle">
+          <svg>
+            <circle cx={70} cy={70} r={70}></circle>
+            <circle id="proc" cx={70} cy={70} r={70}></circle>
+          </svg>
+          <div className="number">
+            <h2>70%</h2>
+          </div>
+        </div>
       </div>
     </div>
   );
